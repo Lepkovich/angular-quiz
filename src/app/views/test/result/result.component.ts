f9fe77f4b88afc2fd9484f003e425168d90a01b3
Lepkovich <6332133@gmail.com>
2023-12-10 14:00:46 +0300

    lesson is done

--- src/app/views/test/result/result.component.ts
+++ src/app/views/test/result/result.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TestService} from "../../../shared/services/test.service";
+import {ActivatedRoute} from "@angular/router";
+import {AuthService} from "../../../core/auth/auth.service";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {PassTestResponseType} from "../../../../types/pass-test-response.type";
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss']
 })
-export class ResultComponent {
+export class ResultComponent implements OnInit{
 
+  result: string = '';
+
+  constructor(private testService: TestService,
+              private activatedRoute: ActivatedRoute,
+              private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    const userInfo = this.authService.getUserInfo();
+    if (userInfo) {
+      this.activatedRoute.queryParams
+        .subscribe(param => {
+          if(param['id']) {
+            this.testService.getResult(param['id'], userInfo.userId)
+              .subscribe(result => {
+                if(result) {
+                  if ((result as DefaultResponseType).error !== undefined) {
+                    throw new Error((result as DefaultResponseType).message);
+                  }
+                  this.result = (result as PassTestResponseType).score + '/' + (result as PassTestResponseType).total;
+                }
+              })
+          }
+        })
+    }
+  }
 }
